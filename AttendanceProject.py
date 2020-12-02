b84261987d46ca7c7805c23fa691ba1740db4ef1
soumyb09 <58508654+soumyb09@users.noreply.github.com>
2020-12-02 18:18:27 +0530

    Adding attendance related changes with data

---
+++ AttendanceProject.py
@@ -0,0 +1,81 @@
+import face_recognition
+import cv2
+import numpy as np
+import os
+from datetime import datetime
+import namelist
+
+path = 'ImagesAttendance'
+images = []     # LIST CONTAINING ALL THE IMAGES
+className = []    # LIST CONTAINING ALL THE CORRESPONDING CLASS Names
+myList = os.listdir(path)
+print("Total Classes Detected:",len(myList))
+for x,cl in enumerate(myList):
+        curImg = cv2.imread(f'{path}/{cl}')
+        images.append(curImg)
+        className.append(os.path.splitext(cl)[0])
+
+
+
+def findEncodings(images):
+    encodeList = []
+    for img in images:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)[0]
+        encodeList.append(encode)
+    return encodeList
+
+def markAttendance(name):
+    with open('Attendance.csv','r+') as f:
+        myDataList = f.readlines()
+        nameList =[]
+        for line in myDataList:
+            entry = line.split(',')
+            nameList.append(entry[0])
+        if name not in  nameList:
+            now = datetime.now()
+            dt_string = now.strftime("%H:%M:%S")
+            f.writelines(f'\n{name},{dt_string}')
+
+
+encodeListKnown = findEncodings(images)
+print('Encodings Complete')
+
+cap = cv2.VideoCapture(0)
+
+
+
+while True:
+    success, img = cap.read()
+    imgS = cv2.resize(img, (0, 0), fx=0.25, fy=0.25)
+    imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
+
+    facesCurFrame = face_recognition.face_locations(imgS)
+    encodesCurFrame = face_recognition.face_encodings(imgS, facesCurFrame)
+
+    for encodeFace, faceLoc in zip(encodesCurFrame, facesCurFrame):
+        matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
+        faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
+        faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
+        # print(faceDis)
+        matchIndex = np.argmin(faceDis)
+
+
+        if matches[matchIndex]:
+            name = className[matchIndex].upper()
+            y1, x2, y2, x1 = faceLoc
+            y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
+            cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+            cv2.rectangle(img, (x1, y2 - 35), (x2, y2), (0, 255, 0), cv2.FILLED)
+            cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_DUPLEX, 1.0, (255, 255, 255), 1)
+            markAttendance(name)
+
+    cv2.imshow('Video', img)
+
+    # Hit 'q' on the keyboard to quit!
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+ # Release handle to the webcam
+video_capture.release()
+cv2.destroyAllWindows()
